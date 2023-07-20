eb5a12c3099f08b7006bf5fa8dc0548b3374bb62
njagiiii <grace.makena@student.moringaschool.com>
2023-07-20 12:19:00 -0400

    children-revised

--- src/components/App.js
+++ src/components/App.js
@@ -4,16 +4,14 @@ import Container from "./Container";
 function App() {
   return (
     <div className="App">
-      <Container header="Example header" direction="horizontal">
+      <Container header="Example header" direction="horizontal" textPosition="center">
         <div>This is an example!</div>
         <div>This is an example!</div>
         <div>This is an example!</div>
-        <div>This is an example!</div>
-        <div>This is an example!</div>
-        <div>This is an example!</div>
-        <div>This is an example!</div>
-      </Container>
-      <Container header="Example header" textPosition="center">
+        
+        </Container>
+
+        <Container header="Example header" textPosition="center">
         <img
           alt="turtle"
           src="https://pbs.twimg.com/profile_images/3022993403/fdae67d800527328cee7a222f048dbbd.jpeg"
@@ -31,7 +29,8 @@ function App() {
           src="https://pbs.twimg.com/profile_images/3022993403/fdae67d800527328cee7a222f048dbbd.jpeg"
         />
       </Container>
-    </div>
+      </div>
+  
   );
 }
 
